042ed9523981d0423be22349180daf3d3a3d7a52
John Cudd <jmcudd@gmail.com>
2020-04-08 14:56:36 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+'use strict';
+
+var chalk = require('chalk');
+var seedrandom = require("seedrandom");
+
+function doy() {
+  var now = new Date();
+  var start = new Date(now.getFullYear(), 0, 0);
+  var diff = now - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.floor(diff / oneDay);
+  return day;
+}
+
+function shuffle(array) {
+  var rng = seedrandom(String(doy()));
+  var currentIndex = array.length,
+    temporaryValue,
+    randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(rng() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+var items=process.argv.slice(2).sort();
+shuffle(items);
+console.log(items);
+items.forEach((item, i)=>{
+  console.log(`${i+1}.${chalk.blue(item)}`)
+})
